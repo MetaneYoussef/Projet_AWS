f7a44cbc4a73c1b2709d91e7d762629c736b97b2
Sarra Brahem <sarahbrahem5@gmail.com>
2024-03-25 21:40:00 +0100

    update user profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,55 +14,61 @@ import MovieHomePage from "./pages/Genres/Films/AccueilFilm";
 import SeriesHomePage from "./pages/Genres/Series/AccueilSeries";
 import MovieDetailPage from "./pages/Genres/Films/MovieDetailPage";
 import SeriesDetailPage from "./pages/Genres/Series/SeriesDetailPage";
-<<<<<<< HEAD
-import UserProfile from "./pages/Divers/UserProfile";
-
-=======
 import FormulaireInscription from "./pages/FormulaireInscription/FormulaireInscription";
-import UserProfile from "./pages/UserProfile/UserProfile";  
->>>>>>> 84845a46dd9d7a9e7a213e7c9508fd899b4e4046
+import UserProfile from "./pages/UserProfile/UserProfile";
+
 
 function App() {
-  return (
-    <Router>
-<<<<<<< HEAD
-        <Routes>
-          <Route path="/" element={<Accueil/>} />
-          <Route path="/connexion" element={<Connexion />} />
-          <Route path="/films" element={<MovieHomePage />} />
-          <Route path="/films/:genre" element={<FilmsPage />} />
-          <Route path="/films/detail/:movieId" element={<MovieDetailPage />} />
-          <Route path="/series" element={<SeriesHomePage />} />
-          <Route path="/series/:genre" element={<Series />} />
-          <Route path="/series/detail/:seriesId" element={<SeriesDetailPage />} />
-          <Route path="/qcm" element={<QCM />} />
-          <Route path="/Evenement" element={<Evenement />} />
-          <Route path="/Inscription" element={<Signup />} />
-          <Route path="/Dune" component={<Dune />} />
-          <Route path="/UserProfile" component={<UserProfile />} />
-        </Routes>
-        {/* Vous pouvez ajouter plus de sections ou de contenu ici */}
-=======
-      <Routes>
-        <Route path="/" element={<Accueil />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/films" element={<MovieHomePage />} />
-        <Route path="/films/:genre" element={<FilmsPage />} />
-        <Route path="/films/detail/:movieId" element={<MovieDetailPage />} />
-        <Route path="/series" element={<SeriesHomePage />} />
-        <Route path="/series/:genre" element={<Series />} />
-        <Route path="/series/detail/:seriesId" element={<SeriesDetailPage />} />
-        <Route path="/qcm" element={<QCM />} />
-        <Route path="/Evenement" element={<Evenement />} />
-        <Route path="/Inscription" element={<Signup />} />
-        <Route path="/FormulaireInscription" element={<FormulaireInscription />} />
-        <Route path="/UserProfile" element={<UserProfile />} />
-        <Route path="/Dune" component={<Dune />} />
-      </Routes>
-      {/* Vous pouvez ajouter plus de sections ou de contenu ici */}
->>>>>>> 84845a46dd9d7a9e7a213e7c9508fd899b4e4046
-    </Router>
-  );
+    return ( <
+        Router >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Accueil / > }
+        /> <
+        Route path = "/connexion"
+        element = { < Connexion / > }
+        /> <
+        Route path = "/films"
+        element = { < MovieHomePage / > }
+        /> <
+        Route path = "/films/:genre"
+        element = { < FilmsPage / > }
+        /> <
+        Route path = "/films/detail/:movieId"
+        element = { < MovieDetailPage / > }
+        /> <
+        Route path = "/series"
+        element = { < SeriesHomePage / > }
+        /> <
+        Route path = "/series/:genre"
+        element = { < Series / > }
+        /> <
+        Route path = "/series/detail/:seriesId"
+        element = { < SeriesDetailPage / > }
+        /> <
+        Route path = "/qcm"
+        element = { < QCM / > }
+        /> <
+        Route path = "/Evenement"
+        element = { < Evenement / > }
+        /> <
+        Route path = "/Inscription"
+        element = { < Signup / > }
+        /> <
+        Route path = "/FormulaireInscription"
+        element = { < FormulaireInscription / > }
+        /> <
+        Route path = "/UserProfile"
+        element = { < UserProfile / > }
+        /> <
+        Route path = "/Dune"
+        component = { < Dune / > }
+        /> < /
+        Routes > <
+        /Router>
+    );
 }
 
 export default App;
