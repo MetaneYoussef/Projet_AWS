1e35d7bcbea11d57cd990cf30397b9c4910f05d7
Arun <arun.ballgobin@gmail.com>
2024-03-30 00:54:43 +0100

    petits ajustements

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,44 +5,52 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <><div>
-      <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-3 gap-8 text-left">
-          {/* Section Logo et Sélection de la langue */}
-          <div className="flex flex-col items-center md:items-start">
-            <img src={`${process.env.PUBLIC_URL}/images/RondAvecFond.png`} alt="Logo" className="h-28 mb-4" />
-            <select className="bg-black text-white p-2 rounded cursor-pointer">
-              <option value="fr">Français</option>
-              <option value="en">English</option>
-            </select>
+      <div className="container h-30 mx-auto flex flex-col md:flex-row justify-between items-center">
+        {/* Section Logo et Nom */}
+        <div className="mb-4 md:mb-0 flex flex-col md:flex-row items-center">
+          <div>
+            <img src={`${process.env.PUBLIC_URL}/images/RondAvecFond.png`} className="h-28 ml-14 mr-10" />
+            <br></br>
+            <div className="relative">
+              <select className="bg-black text-white p-2 rounded items-center cursor-pointer ml-16">
+                <option value="fr">Français</option>
+                <option value="en">English</option>
+              </select>
           </div>
-          {/* Liens Entreprise */}
-          <div className="flex flex-col items-center md:items-start">
-            <p className="font-bold text-gray-500 text-xs mb-3">ENTREPRISE</p>
+          </div>
+          <div className="ml-4 pt-12 justify-center flex flex-col font-bold items-center">
+            <p className="mb-3 md:mb-3 md:mx-2 text-gray-500 text-xs" >ENTREPRISE</p>
             <Link to="/A-Propos" className="mb-3 md:mb-3 md:mx-2 hover:underline">À propos</Link>
             <Link to="/Creators" className="mb-3 md:mb-3 md:mx-2 hover:underline">Créateurs</Link>
             <Link to="/privacy-policy" className="mb-3 md:mb-3 md:mx-2 hover:underline">Confidentialité</Link>
             <Link to="/FAQ" className="mb-2 md:mb-3 md:mx-2 hover:underline">FAQ</Link>
             <Link to="/CGU" className="mb-2 md:mb-3 md:mx-2 hover:underline">CGU</Link>
-            </div>
-          {/* Liens Utiles et Nous Suivre */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="flex flex-col items-center md:items-start">
-              <p className="font-bold text-gray-500 text-xs mb-3">LIENS UTILES</p>
+          </div>
+          <div className="ml-4 pt-12 justify-center flex flex-col font-bold items-center mb-14">
+            <p className="mb-3 md:mb-3 md:mx-2 text-gray-500 text-xs" >LIENS UTILES</p>
             <Link to="/A-Propos" className="mb-3 md:mb-3 md:mx-2 hover:underline">Support</Link>
             <Link to="/Creators" className="mb-3 md:mb-3 md:mx-2 hover:underline">Contact</Link>
-            </div>
-            <div className="flex flex-col items-center md:items-start">
-              <p className="font-bold text-gray-500 text-xs mb-3">NOUS SUIVRE</p>
+          </div>
+          <div className="ml-4 justify-center flex flex-col font-bold items-center mb-5">
+            <p className="mb-3 md:mb-3 md:mx-2 text-gray-500 text-xs" >NOUS SUIVRE</p>
             <Link to="Facebook" className="mb-3 md:mb-3 md:mx-2 hover:underline">Facebook</Link>
             <Link to="/Twitter" className="mb-3 md:mb-3 md:mx-2 hover:underline">Twitter</Link>
             <Link to="/Instragram" className="mb-3 md:mb-3 md:mx-2 hover:underline">Instagram</Link>
-            </div>
           </div>
         </div>
+
+
+        
       </div>
-      <footer className="bg-black text-white text-center p-5">
-        <p>&copy; {new Date().getFullYear()}, What You Watched. All rights reserved.</p>
-      </footer>
-    </>
+    </div><div>
+        <div>
+          <footer className="h-15 flex justify-center bg-black text-white text-center p-5 mt-8">
+            <p>&copy; {new Date().getFullYear()}, What You Watched. All rights reserved.</p>
+          </footer>
+        </div>
+      </div></>
   );
 }
+
+
 export default Footer;
