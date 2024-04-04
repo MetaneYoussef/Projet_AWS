3a2f90a82ca90811bf5efa105e122130ef65db27
Jermissen <jemsen78@hotmail.com>
2024-04-04 13:40:16 +0200

    403 Page + Sections Watchlist

--- frontend/src/pages/Divers/Page403.jsx
+++ frontend/src/pages/Divers/Page403.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Error404Page() {
+function Error403Page() {
   return(
     <div>
       <body class="flex flex-col h-screen justify-center items-center bg-gray-100">
         <div class="flex flex-col items-center">
-          <h1 class="text-[240px] font-extrabold text-gray-500">404</h1>
-          <p class="text-2xl font-medium text-gray-600 mb-6">Page Not Found</p>
+          <h1 class="text-[240px] font-extrabold text-gray-500">403</h1>
+          <p class="text-2xl font-medium text-gray-600 mb-6">ACCES NON AUTORISÉ !</p>
           <a href="/"
             class="px-4 py-2 font-medium text-white bg-indigo-500 rounded-md hover:bg-indigo-600 transition-all duration-200 ease-in-out">
             Retourner sur la page d'accueil
@@ -17,4 +17,4 @@ function Error404Page() {
   );
 };
 
-export default Error404Page;
+export default Error403Page;
