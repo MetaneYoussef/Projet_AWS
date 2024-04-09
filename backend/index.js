36f596eb8cb873e1bd73d3398c8114fb74e75227
Youcef <mtn.youssef.kh@gmail.com>
2024-04-10 00:25:18 +0200

    another try

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-    origin: ["https://what-you-watched.vercel.app/", "http://localhost:3000", "http://localhost:4000"],
+    origin: ["https://what-you-watched.vercel.app/", "https://what-you-watched-backend.vercel.app/", "http://localhost:3000", "http://localhost:4000"],
     methods: ["POST", "GET", "PATCH"],
     credentials: true
 }));
