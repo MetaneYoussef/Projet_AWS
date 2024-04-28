dda38510ce95ce0ba68485adfa2ea9b63724c35b
Arun <arun.ballgobin@gmail.com>
2024-04-28 21:30:46 +0200

    deconnexion effective

--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useCallback } from 'react';
-import Select from 'react-select'; // Import de react-select
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import { useAuth } from '../../context/AuthContext';
@@ -8,16 +7,11 @@ import { Link } from 'react-router-dom';
 function UserProfile() {
   const [userPreferences, setUserPreferences] = useState({
       username: "AWS Projet",
-      favoriteGenres: "",
-      favoriteDirectors: "",
       email: "AWSProjet@email.com", 
       password: ""
   });
-  const [customGenre, setCustomGenre] = useState('');
-  const [customDirector, setCustomDirector] = useState('');
   const [errors, setErrors] = useState({});
 
-  const directors = ["Quentin Tarantino", "Christopher Nolan", "Greta Gerwig", "Bong Joon-ho", "Autre"];
 
   const { logout } = useAuth();
 
@@ -41,16 +35,7 @@ function UserProfile() {
       validateField(name, value);
   }, [validateField]);
 
-  const handleCustomChange = useCallback((e) => {
-      const { name, value } = e.target;
-      if (name === 'customGenre') {
-          setCustomGenre(value);
-          setUserPreferences(prevState => ({ ...prevState, favoriteGenres: "Autre", customGenre: value }));
-      } else if (name === 'customDirector') {
-          setCustomDirector(value);
-          setUserPreferences(prevState => ({ ...prevState, favoriteDirectors: "Autre", customDirector: value }));
-      }
-  }, []);
+  
 
   const handleSubmit = async (e) => {
       e.preventDefault();
@@ -63,14 +48,7 @@ function UserProfile() {
   };
 
   const [showChangePasswordModal, setShowChangePasswordModal] = useState(false);
-  const genresOptions = ["Action", "Comédie", "Animation", "Émotion", "Jeunesse", "Horreur", "Science-Fiction", "Suspense"].map(genre => ({ value: genre, label: genre }));
-
-  const handleGenreChange = selectedOptions => {
-    setUserPreferences(prevState => ({
-      ...prevState,
-      favoriteGenres: selectedOptions.map(option => option.value)
-    }));
-  };
+  
 
 
   const handleChangePasswordSubmit = e => {
@@ -130,18 +108,7 @@ function UserProfile() {
                 )}
 
 
-                {/* Sélecteur de genres avec tags */}
-                <div className='relative z-0'>
-                  <label>Genres favoris</label>
-                  <Select
-                    isMulti
-                    name="genres"
-                    options={genresOptions}
-                    className="basic-multi-select"
-                    classNamePrefix="select"
-                    onChange={handleGenreChange}
-                  />
-                </div>
+               
 
                 {/* Bouton de soumission des modifications */}
                 <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
