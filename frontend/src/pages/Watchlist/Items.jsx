782cdb2ae2d0c737311513014cf253f55d7d4307
Jermissen <jemsen78@hotmail.com>
2024-05-03 06:34:37 +0200

    Retour a la Watchlist De Base

--- frontend/src/pages/Watchlist/Items.jsx
+++ frontend/src/pages/Watchlist/Items.jsx
@@ -1,28 +1,6 @@
 import React from 'react';
-import axios from 'axios';
-import { useAuth } from "../../context/AuthContext";
 
 function WatchlistItem({ id, title, episodeInfo, rating, poster, type }) {
-  const { user } = useAuth(); 
-
-  const handleAddToWatchlist = async () => {
-    const endpoint = type === 'film' ? `/api/filmsWatchlist` : `/api/seriesWatchlist`;
-    const data = {
-			tmdbId: id,
-			userId: user.id,
-			...(type === 'serie' ? { saison: episodeInfo.saison, episode: episodeInfo.episode } : {})
-		};
-		
-    try {
-      await axios.post(endpoint, data, {
-        headers: { Authorization: `Bearer ${user.token}` }
-      });
-      alert(`Ajouté à la Watchlist de ${type}!`);
-    } catch (error) {
-      console.error('Erreur lors de l\'ajout à la Watchlist', error);
-      alert("Erreur lors de l'ajout à la Watchlist.");
-    }
-  };
 
   return (
     <div className="bg-black text-white border-2 rounded-lg flex lg:mb-2">
@@ -31,9 +9,6 @@ function WatchlistItem({ id, title, episodeInfo, rating, poster, type }) {
         <div>
           <p className="text-lg md:text-xl font-bold mb-1 mt-2">{title}</p>
           <p className="text-sm">{episodeInfo}</p>
-          <button onClick={handleAddToWatchlist} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Ajouter à la Watchlist
-          </button>
         </div>
         <div>
           <p className="flex items-center -mt-12 text-lg">
